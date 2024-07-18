da3a37f24b5590458eb68f08a163c4e6e2917b20
isaacscotts <kayimaisaac03@gmail.com>
2024-07-19 00:17:10 +0300

    Update database.py

--- myapp/database.py
+++ myapp/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from myapp.config import settings
 from sqlalchemy.orm import sessionmaker
-SQLALCHEMY_DATABASE_URL=f"mysql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}"
+SQLALCHEMY_DATABASE_URL=f"postgres://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}"
 engine=create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal= sessionmaker(autoflush=False,bind=engine)
 Base= declarative_base()
